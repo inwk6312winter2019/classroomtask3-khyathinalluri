11982a6418fc4d1226b210170e4cfd7b26268f1f
Khyathinalluri <Khyathi.Nalluri@dal.ca>
2019-02-04 07:39:55 -0800

    updated cards to include prototype for hand

--- card.py
+++ card.py
@@ -8,6 +8,22 @@ class card():
 	def __str__(self):
 		return f"the rank is {card.rank_names[self.rank]} and the suite is {card.suit_names[self.suite]}"
 
+	def __it__(self, other):
+		t1 = self.suit, self.rank
+		t2 = other.suit, other.rank
+		return cmp(t1, t2)
+
+	def __eq__(self,other):
+		return self.rank == other.rank and self.suit == other.suit
+class Deck(object):
+	def __init__(self):
+		self.cards = []
+		for suit in range(4):
+			for rank in range(1, 14):
+				card = Card(suit, rank)
+				self.cards.append(card)
+	def pop_card(self):
+		return self.card.pop()
 
 ace_of_spade=card(1,3)
 print(ace_of_spade)
